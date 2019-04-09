430d9d02ef9fdab60ec81d8622c74bdedffc270a
grudus <jakgruda@gmail.com>
2019-04-09 20:28:05 +0200

    Accept only input file as argument and loop over each heuristic

--- src/main/kotlin/pwr/csp/Launcher.kt
+++ src/main/kotlin/pwr/csp/Launcher.kt
@@ -9,16 +9,12 @@ import pwr.csp.algorithm.heuristic.BoardPointSelector
 import pwr.csp.algorithm.heuristic.MostLimitingBoardPointSelector
 import pwr.csp.algorithm.heuristic.RandomBoardPointSelector
 import pwr.csp.games.Game
+import pwr.csp.games.PossibleValuesGame
 import pwr.csp.reader.FutoshikiReader
 import pwr.csp.reader.GameReader
 import pwr.csp.reader.SudokuReader
 import java.io.File
 import java.util.Arrays.toString
-
-private enum class GameStrategy(val reader: GameReader<out Game>) {
-    SUDOKU(SudokuReader()),
-    FUTOSHIKI(FutoshikiReader()),
-}
 
 private enum class SolverStrategy(val solver: Solver) {
     FORWARD_CHECKING(ForwardCheckingSolver()),
@@ -27,39 +23,42 @@ private enum class SolverStrategy(val solver: Solver) {
 
 private enum class ValueSelectorStrategy(val boardPointSelector: BoardPointSelector) {
     MOST_CONSTRAINED(MostConstrainedBoardPointSelector()),
-    RANDOM(RandomBoardPointSelector()),
     MOST_LIMITING(MostLimitingBoardPointSelector()),
+    RANDOM(RandomBoardPointSelector()),
 }
 
 fun main(args: Array<String>) {
-    if (args.size < 4) {
-        println("Program expects 4 arguments: ")
-        println("1) Reader: One of ${toString(GameStrategy.values())}")
-        println("2) Solver: One of ${toString(SolverStrategy.values())}")
-        println("3) Value selector: One of ${toString(ValueSelectorStrategy.values())}")
-        println("4) File with data to solve")
+    if (args.isEmpty()) {
+        println("Specify input file with puzzle to solve")
         return
     }
 
     println("__ Hello in the Constraint Satisfaction Problem __")
 
-    val reader = GameStrategy.valueOf(args[0]).reader
-    val solver = SolverStrategy.valueOf(args[1]).solver
-    val valueSelector = ValueSelectorStrategy.valueOf(args[2]).boardPointSelector
-    val file = File(args[3])
+    val reader: GameReader<out Game> =
+            if (args[0].contains("udoku"))
+                SudokuReader()
+            else FutoshikiReader()
 
+    val file = File(args[0])
     val game: Game = reader.read(file)
 
     println("Results for file: $file")
-    println("Solver:  ${solver.javaClass.simpleName}")
-    println("Value selector: ${valueSelector.javaClass.simpleName}")
-    println()
 
-    val solutionDescription: SolutionDescription = GameSolutionsFinder.findSolutions(
-            game,
-            solver,
-            valueSelector
-    )
+    SolverStrategy.values().forEach { solver ->
+        ValueSelectorStrategy.values().forEach { selector ->
 
-    println(solutionDescription)
+            println("Solver:  ${solver.name}")
+            println("Value selector: ${selector.name}")
+            println()
+
+            val solutionDescription: SolutionDescription = GameSolutionsFinder.findSolutions(
+                    game,
+                    solver.solver,
+                    selector.boardPointSelector
+            )
+
+            println(solutionDescription)
+        }
+    }
 }

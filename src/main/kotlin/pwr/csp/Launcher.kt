c19aa399e95f10ec8704a28449c8756e58aa8186
grudus <jakgruda@gmail.com>
2019-04-09 18:58:28 +0200

    Take execution parameters from program arguments

--- src/main/kotlin/pwr/csp/Launcher.kt
+++ src/main/kotlin/pwr/csp/Launcher.kt
@@ -1,35 +1,58 @@
 package pwr.csp
 
 import pwr.csp.algorithm.ForwardCheckingSolver
+import pwr.csp.algorithm.Solver
 import pwr.csp.algorithm.heuristic.MostConstrainedValueSelector
+import pwr.csp.algorithm.heuristic.ValueSelector
+import pwr.csp.games.Game
 import pwr.csp.reader.FutushikiReader
+import pwr.csp.reader.GameReader
 import pwr.csp.reader.SudokuReader
 import java.io.File
+import java.util.*
+import java.util.Arrays.toString
 
-fun main() {
+private enum class GameStrategy(val reader: GameReader<out Game>) {
+    SUDOKU(SudokuReader()),
+    FUTOSHIKI(FutushikiReader())
+}
+
+private enum class SolverStrategy(val solver: Solver) {
+    FORWARD_CHECKING(ForwardCheckingSolver())
+}
+
+private enum class ValueSelectorStrategy(val valueSelector: ValueSelector) {
+    MOST_CONSTRAINED(MostConstrainedValueSelector())
+}
+
+fun main(args: Array<String>) {
+    if (args.size < 4) {
+        println("Program expects 4 arguments: ")
+        println("1) Reader: One of ${toString(GameStrategy.values())}")
+        println("2) Solver: One of ${toString(SolverStrategy.values())}")
+        println("3) Value selector: One of ${toString(ValueSelectorStrategy.values())}")
+        println("4) File with data to solve")
+        return
+    }
+
     println("__ Hello in the Constraint Satisfaction Problem __")
-    val sudokuFile = File("res/test_data/sudoku/test_2")
 
-    val sudoku = SudokuReader().read(sudokuFile)
-    sudoku.printBoard()
+    val reader = GameStrategy.valueOf(args[0]).reader
+    val solver = SolverStrategy.valueOf(args[1]).solver
+    val valueSelector = ValueSelectorStrategy.valueOf(args[2]).valueSelector
+    val file = File(args[3])
+
+    val game: Game = reader.read(file)
+    game.printBoard()
 
     println("\n")
 
-    GameSolutionsFinder.findSolutions(
-            sudoku,
-            ForwardCheckingSolver(),
-            MostConstrainedValueSelector()
-    ).forEach { it.printBoard() }
+    val solutions: List<Game> = GameSolutionsFinder.findSolutions(
+            game,
+            solver,
+            valueSelector
+    )
 
-    val futushikiFile = File("res/research_data/futoshiki/test_futo_4_0.txt")
-
-    val futoshiki = FutushikiReader().read(futushikiFile)
-    futoshiki.printBoard()
-
-
-    GameSolutionsFinder.findSolutions(
-            futoshiki,
-            ForwardCheckingSolver(),
-            MostConstrainedValueSelector()
-    ).forEach { it.printBoard() }
+    println("Found ${solutions.size} solutions:\n")
+    solutions.forEach {it.printBoard()}
 }
